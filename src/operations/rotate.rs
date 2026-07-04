ffecc8a51f545bcdfd04d628068659bc94146072
Hannes Kaeufler <hannes.kaeufler@gmail.com>
2026-07-04 11:47:29 +0200

    Support the currently needed rotation values

--- src/operations/rotate.rs
+++ src/operations/rotate.rs
@@ -1,6 +1,21 @@
-use crate::{arg_parsers::Rotation, error::MagickError, image::Image};
+use crate::{arg_parsers::Rotation, error::MagickError, image::Image, wm_err};
 
-pub fn rotate(image: &mut Image, _rotation: &Rotation) -> Result<(), MagickError> {
-    image.pixels = image.pixels.rotate90();
-    Ok(())
+pub fn rotate(image: &mut Image, rotation: &Rotation) -> Result<(), MagickError> {
+    match rotation {
+        Rotation::Clockwise(num) => {
+            if *num == 90 {
+                image.pixels = image.pixels.rotate90();
+                Ok(())
+            } else if *num == 180 {
+                image.pixels = image.pixels.rotate180();
+                Ok(())
+            } else if *num == 270 {
+                image.pixels = image.pixels.rotate270();
+                Ok(())
+            } else {
+                Err(wm_err!("Unsupported rotation value"))
+            }
+        }
+        Rotation::CounterClockwise(_) => Err(wm_err!("Counterclockwise rotation is not supported")),
+    }
 }
